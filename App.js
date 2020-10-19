d9c4e35e6267af0485ea9cf5720884bde14e3044
Rui Luis <ruidcluis@gmail.com>
2020-10-19 20:11:07 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useRef} from 'react';
+import { StyleSheet, Text, View, TextInput, Vibration, Button, Switch } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Alarm/>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+const Alarm = () => {
+  // For the testing part!
+  const [value, onChangeText] = useState('Write your message here!');
+
+  // For the switch part!
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  var timer = useRef();
+
+  const toggleSwitch = () => {
+
+    if (!isEnabled) {
+      setIsEnabled(true);
+      timer.current = setInterval(() => {
+        Vibration.vibrate(); 
+        alert(value);
+      }, 3000);
+    } else { 
+      timer.current = clearInterval(timer.current)
+      setIsEnabled(false);
+    }
+  }
+
+  return(
+  <View>
+    <TextInput style={styles.messageText} onChangeText={text => onChangeText(text)} value={value} />
+
+    <Button onPress={
+      () => { 
+       Vibration.vibrate();
+       alert(value);
+      }
+    } 
+      title="TEST"/>
+
+      <Switch 
+      style={styles.alarmSwitch} 
+      value={isEnabled}
+      onValueChange={toggleSwitch}
+      />   
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  alarmSwitch: {
+    margin: 40,
+    alignSelf: 'center'
+  },
+  messageText: {
+    textAlign: 'center',
+  }
 });
